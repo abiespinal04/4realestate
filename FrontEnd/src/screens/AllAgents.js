764aee71d3bc26da01f08053b87e5d8248bbfea6
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-19 16:23:24 -0400

    fixed the overall look

--- FrontEnd/src/screens/AllAgents.js
+++ FrontEnd/src/screens/AllAgents.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import AgentCard from "../components/AgentCard";
-import * as action from '../store/actions'
+import * as action from "../store/actions";
 import Profile from "../components/Profile";
+import "../CSS/AllAgents.css";
 
 class AllAgents extends Component {
   state = {
@@ -14,35 +15,35 @@ class AllAgents extends Component {
   async componentDidMount() {
     const { data } = await axios.get("http://localhost:3000/AgentList");
     console.log("Inside agentScreen componentDidMount: ", data.agentList);
-    this.props.LoadAgents(data.agentList)
-    console.log("Data coming from load student  reducer",this.props.AgentList)
-    this.setState({ agentList:data.agentList,});
-
+    this.props.LoadAgents(data.agentList);
+    console.log("Data coming from load student  reducer", this.props.AgentList);
+    this.setState({ agentList: data.agentList });
   }
 
   handleAgentRendering = () => {
     return this.state.agentList.map((agent, index) => (
-        <AgentCard 
-        agent={agent}
-        index={index}
-        /> 
+      <AgentCard agent={agent} index={index} />
     ));
   };
 
   render() {
     return (
-    <div>
-    {this.handleAgentRendering()}
-    </div>);
+      <div id="agentPage">
+        <h1>All Agents</h1>
+        <div>{this.handleAgentRendering()}</div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+  return {
+    AgentList: state.AgentList,
+    Agent: state.SingleUserReducer
+  };
+};
 
-  return{
-    AgentList : state.AgentList,
-    Agent : state.SingleUserReducer
-  }
-}
-
-export default connect(mapStateToProps,action)(AllAgents);
+export default connect(
+  mapStateToProps,
+  action
+)(AllAgents);

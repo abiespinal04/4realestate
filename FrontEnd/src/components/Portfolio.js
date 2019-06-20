88d95b2db2fa33eeed675354a81fdb3e1717bc0e
Abimael Espinal <abimaelespinal@gmail.com>
2019-06-19 21:05:41 -0400

    added delete

--- FrontEnd/src/components/Portfolio.js
+++ FrontEnd/src/components/Portfolio.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 class Portfolio extends Component {
   state = {};
   render() {
+
+
+    
+
     return (
       <div>
         <h1>{this.props.title}</h1>
@@ -12,12 +16,17 @@ class Portfolio extends Component {
          {this.props.agent.type === 'agent' ? 
         <p>
           {this.props.clients.map(client => (
+            <div>
             <h4>{client.firstName}</h4>
-         
+            <button><i class="fas fa-trash-alt"></i></button>
+            </div>
+            
           ))}
             </p>
              : <p>{this.props.clients.map(agent => (
+                <div>
                 <h4>{agent.firstName}</h4>
+                </div>
              ))}</p>}   
             </div>
           <p>Clients:{this.props.clients.length}</p>

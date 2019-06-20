0ef2ca8432ab01d2895e808678ce468378eb42a1
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-19 21:21:18 -0400

    changed the API_URL

--- FrontEnd/src/screens/AllAgents.js
+++ FrontEnd/src/screens/AllAgents.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import AgentCard from "../components/AgentCard";
-import * as action from '../store/actions'
+import * as action from "../store/actions";
 import Profile from "../components/Profile";
+import { API_URL } from "../utilities/API_URL";
 
 class AllAgents extends Component {
   state = {
@@ -12,37 +13,33 @@ class AllAgents extends Component {
   };
 
   async componentDidMount() {
-    const { data } = await axios.get("http://localhost:3000/AgentList");
+    const { data } = await axios.get(`${API_URL}AgentList`);
     console.log("Inside agentScreen componentDidMount: ", data.agentList);
-    this.props.LoadAgents(data.agentList)
-    console.log("Data coming from load student  reducer",this.props.AgentList)
-    this.setState({ agentList:data.agentList,});
-
+    console.log(API_URL + " APIIIIIIIII");
+    this.props.LoadAgents(data.agentList);
+    console.log("Data coming from load student  reducer", this.props.AgentList);
+    this.setState({ agentList: data.agentList });
   }
 
   handleAgentRendering = () => {
     return this.state.agentList.map((agent, index) => (
-        <AgentCard 
-        agent={agent}
-        index={index}
-        /> 
+      <AgentCard agent={agent} index={index} />
     ));
   };
 
   render() {
-    return (
-    <div>
-    {this.handleAgentRendering()}
-    </div>);
+    return <div>{this.handleAgentRendering()}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+  return {
+    AgentList: state.AgentList,
+    Agent: state.SingleUserReducer
+  };
+};
 
-  return{
-    AgentList : state.AgentList,
-    Agent : state.SingleUserReducer
-  }
-}
-
-export default connect(mapStateToProps,action)(AllAgents);
+export default connect(
+  mapStateToProps,
+  action
+)(AllAgents);
---
+++ FrontEnd/src/utilities/API_URL.js
@@ -0,0 +1,3 @@
+export const API_URL = !process.env.Production
+  ? "https://mighty-castle-29675.herokuapp.com/"
+  : "http://localhost:3000/";

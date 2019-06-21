981ef777b7ae874adab7b6f240b547a947494ecc
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-20 22:56:46 -0400

    created nice looking cards in portfolio

--- FrontEnd/src/components/Portfolio.js
+++ FrontEnd/src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as action from "../store/actions";
+import "../CSS/Portfolio.css";
 
 class Portfolio extends Component {
   state = {
@@ -40,61 +41,69 @@ class Portfolio extends Component {
       <div>
         <h1>{this.props.title}</h1>
         <p>{this.props.email}</p>
-        <h4>Clients</h4>
-        <div style={{ textAlign: "center", backgroundColor: "red" }}>
-          {this.props.agent.type === "agent" ? (
-            <div>
-              {this.props.clients.map((client, index) =>
-                client !== null ? (
-                  <div>
-                    <img src={client.imageURL} alt="pic" />
-                    <h4>{client.firstName}</h4>
-                    <h4>{client.lastName}</h4>
-                    <h4>{client.email}</h4>
+        <h1>Clients</h1>
+        {this.props.agent.type === "agent" ? (
+          <div className="clientList">
+            {this.props.clients.map((client, index) =>
+              client !== null ? (
+                <div className="portfolioContainer">
+                  <div className="innerContainer">
+                    <img
+                      className="clientPicture"
+                      src={client.imageURL}
+                      alt="pic"
+                    />
 
-                    {client.address !== undefined ? (
-                      <h4>{client.address.street} </h4>
-                    ) : null}
-                    {client.address !== undefined ? (
-                      <h4>{client.address.city} </h4>
-                    ) : null}
-                    {client.address !== undefined ? (
-                      <h4>{client.address.state} </h4>
-                    ) : null}
-                    {client.house !== undefined ? (
-                      <h4>Total: {client.house.total} </h4>
-                    ) : (
-                      <p>not found</p>
-                    )}
-                    {client.house !== undefined ? (
-                      <h4>Tax year: {client.house.tax_year} </h4>
-                    ) : (
-                      <p>not found</p>
-                    )}
-                    {client.house !== undefined ? (
-                      <h4>Tax amount:{client.house.tax_amount} </h4>
-                    ) : (
-                      <p>not found</p>
-                    )}
-                    {/* <h4>{client.address.city}</h4>
-            <h4>{client.address.state}</h4> */}
-                    <button onClick={() => this.handleDelete(client, index)}>
-                      <i class="fas fa-trash-alt" />
-                    </button>
+                    <div className="theInfo">
+                      <h3>{client.firstName}</h3>
+                      <h3>{client.lastName}</h3>
+                      <p>{client.email}</p>
+
+                      {client.address !== undefined ? (
+                        <h5>{client.address.street} </h5>
+                      ) : null}
+                      {client.address !== undefined ? (
+                        <h5>{client.address.city} </h5>
+                      ) : null}
+                      {client.address !== undefined ? (
+                        <h5>{client.address.state} </h5>
+                      ) : null}
+                      {client.house !== undefined ? (
+                        <p>Total: {client.house.total} </p>
+                      ) : (
+                        <p>not found</p>
+                      )}
+                      {client.house !== undefined ? (
+                        <p>Tax year: {client.house.tax_year} </p>
+                      ) : (
+                        <p>not found</p>
+                      )}
+                      {client.house !== undefined ? (
+                        <p>Tax amount:{client.house.tax_amount} </p>
+                      ) : (
+                        <p>not found</p>
+                      )}
+                    </div>
                   </div>
-                ) : null
-              )}
-            </div>
-          ) : (
-            <p>
-              {this.props.clients.map(agent => (
-                <div>
-                  <h4>{agent.firstName}</h4>
+                  <button
+                    className="profileButton"
+                    onClick={() => this.handleDelete(client, index)}
+                  >
+                    <i className="fas fa-trash-alt" />
+                  </button>
                 </div>
-              ))}
-            </p>
-          )}
-        </div>
+              ) : null
+            )}
+          </div>
+        ) : (
+          <p>
+            {this.props.clients.map(agent => (
+              <div>
+                <h4>{agent.firstName}</h4>
+              </div>
+            ))}
+          </p>
+        )}
         <p>Clients:{this.props.clients.length}</p>
       </div>
     );
